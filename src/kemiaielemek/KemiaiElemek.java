4d3753eb9b1e398cee971712082a5c1f42ef4c38
SiMate99 <sibakmate@gmail.com>
2021-03-03 12:51:07 +0100

    5. 6. Feladat

--- src/kemiaielemek/KemiaiElemek.java
+++ src/kemiaielemek/KemiaiElemek.java
@@ -30,38 +30,16 @@ public class KemiaiElemek {
         System.out.println("4.feladat:");
         for (Felfedezes felfedezes : Felfedezesek) {
             if (felfedezes.getEv().equals("Ókor")) {
-                System.out.println(felfedezes);
+                //System.out.println(felfedezes);
             }
         }
-        /*8.feladat*/
-        System.out.println("8.feladat");
-        HashMap<String, Integer> elemekFelf = new HashMap<>();
-
-        for (Felfedezes felf : Felfedezesek) {
-            if (elemekFelf.containsKey(felf.getEv())) {
-                elemekFelf.put(felf.getEv(), elemekFelf.get(felf.getEv()) + 1);
-            } else {
-                elemekFelf.put(felf.getEv(), 1);
-            }
-        }
-
-        for (Map.Entry<String, Integer> elem : elemekFelf.entrySet()) {
-            String key = elem.getKey();
-            Integer value = elem.getValue();
-            if (value > 3 && isString(key)) {
-                System.out.println(key + " : " + value);
-            }
-
-        }
-
-    
+        
         /*5.Feladat*/
-        System.out.println("5. Feladat");
         Scanner sc = new Scanner(System.in);
         boolean vegyjelE = false;
         String ertek = "";
         while(!vegyjelE){
-            System.out.print("Kérek egy vegyjelet: ");
+            System.out.print("5. Feladat: Kérek egy vegyjelet: ");
             ertek = sc.nextLine();
             if (ertek.length() <= 2) {
                 for (int i = 0; i < ertek.length(); i++) {
@@ -86,12 +64,37 @@ public class KemiaiElemek {
         if (i >= Felfedezesek.size()-1) {
             System.out.println("Nincs ilyen elem az adatforrásban!");
         }else{
-            System.out.println("\tAz elem vegyjele: " + Felfedezesek.get(i).getVegyjel() +
-                        "\n\tAz elem neve: " + Felfedezesek.get(i).getElem() +
-                        "\n\tRendszáma: " + Felfedezesek.get(i).getRendszam() +
-                        "\n\tFelfedezés éve: " + Felfedezesek.get(i).getEv() +
-                        "\n\tFelfedező: " + Felfedezesek.get(i).getFelfedezo());
+            Felfedezes sor = Felfedezesek.get(i);
+            System.out.println("\tAz elem vegyjele: " + sor.getVegyjel() +
+                        "\n\tAz elem neve: " + sor.getElem() +
+                        "\n\tRendszáma: " + sor.getRendszam() +
+                        "\n\tFelfedezés éve: " + sor.getEv() +
+                        "\n\tFelfedező: " + sor.getFelfedezo());
         }
+        
+        /*8.feladat*/
+        System.out.println("8.feladat");
+        HashMap<String, Integer> elemekFelf = new HashMap<>();
+
+        for (Felfedezes felf : Felfedezesek) {
+            if (elemekFelf.containsKey(felf.getEv())) {
+                elemekFelf.put(felf.getEv(), elemekFelf.get(felf.getEv()) + 1);
+            } else {
+                elemekFelf.put(felf.getEv(), 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> elem : elemekFelf.entrySet()) {
+            String key = elem.getKey();
+            Integer value = elem.getValue();
+            if (value > 3 && isString(key)) {
+                System.out.println(key + " : " + value);
+            }
+
+        }
+
+    
+        
         
 
     }

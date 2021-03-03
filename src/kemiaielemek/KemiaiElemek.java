34de0bc5dfef48c9ce6f5d2ca3cb9aefe33e6493
SiMate99 <sibakmate@gmail.com>
2021-03-03 12:29:10 +0100

    5. 6. Feladat

--- src/kemiaielemek/KemiaiElemek.java
+++ src/kemiaielemek/KemiaiElemek.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class KemiaiElemek {
 
@@ -16,7 +17,43 @@ public class KemiaiElemek {
             Felfedezesek.add(new Felfedezes(sor));
         }
         
+        System.out.println("5. Feladat");
+        Scanner sc = new Scanner(System.in);
+        boolean vegyjelE = false;
+        String ertek = "";
+        while(!vegyjelE){
+            System.out.print("Kérek egy vegyjelet: ");
+            ertek = sc.nextLine();
+            if (ertek.length() <= 2) {
+                for (int i = 0; i < ertek.length(); i++) {
+                    char c = ertek.charAt(i);
+                    if (!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z')) {
+                        vegyjelE = false;
+                    }else{
+                        vegyjelE = true;
+                    }
+                }
+                
+            }
+        }
+        //System.out.println(ertek);
         
+        System.out.println("6. Feladat: Keresés");
+        int i = 0;
+        while(i < Felfedezesek.size()-1 && !(Felfedezesek.get(i).getVegyjel().toLowerCase().equals(ertek.toLowerCase()))){
+            i++;
+        }
+        if (i >= Felfedezesek.size()-1) {
+            System.out.println("Nincs ilyen elem az adatforrásban!");
+        }else{
+            System.out.println("\tAz elem vegyjele: " + Felfedezesek.get(i).getVegyjel() +
+                        "\n\tAz elem neve: " + Felfedezesek.get(i).getElem() +
+                        "\n\tRendszáma: " + Felfedezesek.get(i).getRendszam() +
+                        "\n\tFelfedezés éve: " + Felfedezesek.get(i).getEv() +
+                        "\n\tFelfedező: " + Felfedezesek.get(i).getFelfedezo());
+        }
+        
+
     }
     
 }
